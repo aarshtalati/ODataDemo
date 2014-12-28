51eb5910d50b0662c20fbf643991db9191e804a6
aarshtalati <aarshstalati1989@gmail.com>
2014-12-27 19:32:33 -0500

    added odata client using asp.net web api

---
+++ WebApiClient/Controllers/CustomersController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Linq;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.OData;
+using EF;
+
+namespace WebApiClient.Controllers
+{
+	public class CustomersController : ApiController
+	{
+		NorthwindEntities context = new NorthwindEntities();
+
+		// GET: api/Customers
+		[EnableQuery]
+		public IQueryable<Customer> Get()
+		{
+			context.Configuration.ProxyCreationEnabled = false;
+			return context.Customers;//.AsNoTracking();
+		}
+
+		// GET: api/Customers/5
+		public string Get(int id)
+		{
+			context.Configuration.ProxyCreationEnabled = false;
+			return "value";
+		}
+
+		// POST: api/Customers
+		public void Post([FromBody]string value)
+		{
+		}
+
+		// PUT: api/Customers/5
+		public void Put(int id, [FromBody]string value)
+		{
+		}
+
+		// DELETE: api/Customers/5
+		public void Delete(int id)
+		{
+		}
+	}
+}
---
+++ WebApiClient/Controllers/ProductsController.cs
@@ -0,0 +1,44 @@
+﻿using System.Web.Http.OData;
+using EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApiClient.Controllers
+{
+	public class ProductsController : ApiController
+	{
+		NorthwindEntities cotnext = new NorthwindEntities();
+		// GET: api/Products
+		[EnableQuery]
+		public IQueryable<Order> Get()
+		{
+			cotnext.Configuration.ProxyCreationEnabled = false;
+			return cotnext.Orders;
+		}
+
+		// GET: api/Products/5
+		public string Get(int id)
+		{
+			return "value";
+		}
+
+		// POST: api/Products
+		public void Post([FromBody]string value)
+		{
+		}
+
+		// PUT: api/Products/5
+		public void Put(int id, [FromBody]string value)
+		{
+		}
+
+		// DELETE: api/Products/5
+		public void Delete(int id)
+		{
+		}
+	}
+}
